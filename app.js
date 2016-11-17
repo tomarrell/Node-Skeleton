ba8038feacd089ed8decc1f10b0bd3914ef4217b
Tom Arrell <tom.arrell@gmail.com>
2016-11-18 03:16:10 +1300

    Truncate req res

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(app.get('port'), () => {
